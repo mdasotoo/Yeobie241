0417739c0fb6090122025ad0be40d0c5445e495c
Maya Das OToole <mayadasotoole@Mayas-MacBook-Air-2.local>
2020-12-10 17:07:26 -0500

    Adding UI

---
+++ Yeobie/Assets/hero1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hero1 : MonoBehaviour
+{
+    public Sprite hero2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if hero = moving ;
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = hero2;
+        */
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = hero2;
+        transform.Translate(0f, -0.25f, 0f);
+    }
+}
