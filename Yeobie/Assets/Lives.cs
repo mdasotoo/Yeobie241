bc1b27457038d8bb3e0f5369630100e5adebc608
JoeSangiolo <70847265+JoeSangiolo@users.noreply.github.com>
2020-12-11 18:48:48 -0500

    added lives

---
+++ Yeobie/Assets/Lives.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Lives : MonoBehaviour
+{
+    [SerializeField] Text livesText;
+    public int lives;
+    // Start is called before the first frame update
+    void Start()
+    {
+      lives = 3;
+      livesText.text = lives.ToString("0");
+    }
+
+    void CheckLoss() {
+      if (lives == 0) {
+        SceneManager.LoadScene("LostScene");
+      }
+    }
+
+    public void LoseLife() {
+      lives--;
+      livesText.text = lives.ToString("0");
+      CheckLoss();
+    }
+}
